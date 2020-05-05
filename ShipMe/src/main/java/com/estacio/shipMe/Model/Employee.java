410fb2895c908bd11447aad5f1eef3cefe8c7dc5
Waldemario Junior <waldemariojunior@mbpdewaldemario>
2020-05-05 11:50:50 -0300

    changes

--- ShipMe/src/main/java/com/estacio/shipMe/Model/Employee.java
+++ ShipMe/src/main/java/com/estacio/shipMe/Model/Employee.java
@@ -3,10 +3,14 @@ package com.estacio.shipMe.Model;
 public class Employee extends User{
 	
 	String serialNumber;
+	String role;
+	Double salary;
 	
-	public Employee(Long id, String name, UserType type,String serialNumber) {
+	public Employee(Long id, String name, UserType type,String serialNumber, String role, Double salary) {
 		super(id, name, type);
 		this.serialNumber = serialNumber;
+		this.role = role;
+		this.salary = salary;
 	}
 
 	public String getSerialNumber() {
@@ -15,6 +19,26 @@ public class Employee extends User{
 
 	public void setSerialNumber(String serialNumber) {
 		this.serialNumber = serialNumber;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}	
+	
+	public void calcSalary(int daysInMonth, int workedOursPerDay, Double hourValue) {
+		this.salary = (hourValue * workedOursPerDay) * daysInMonth;
 	}
 
 }
--- ShipMe/src/main/java/com/estacio/shipMe/Model/Intern.java
+++ ShipMe/src/main/java/com/estacio/shipMe/Model/Intern.java
@@ -1,13 +1,14 @@
 package com.estacio.shipMe.Model;
 
 import java.sql.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Intern extends Employee{
 	
 	Date contratExpirationDate;
 	
-	public Intern(Long id, String name, UserType type,String serialNumber, Date contratExpirationDate) {
-		super(id, name, type, serialNumber);
+	public Intern(Long id, String name, UserType type,String serialNumber, Date contratExpirationDate, String role, Double salary) {
+		super(id, name, type, serialNumber, role, salary);
 		this.contratExpirationDate = contratExpirationDate;
 	}
 
@@ -17,6 +18,11 @@ public class Intern extends Employee{
 
 	public void setContratExpirationDate(Date contratExpirationDate) {
 		this.contratExpirationDate = contratExpirationDate;
+	}
+	
+	public Long calcExpirantionDate(Date startDate, Date finishDate) {
+		long diffInMillies = Math.abs(startDate.getTime() - finishDate.getTime());
+		return TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
 	}
 
 }
